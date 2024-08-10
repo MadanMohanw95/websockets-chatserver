8a058b93a832070987831b1a926dedac4c2dcc8d
madanmohanw95 <madhanmohanw95@gmail.com>
2024-08-10 01:44:55 -0500

    WebSockets config and controller changes

---
+++ src/main/java/com/example/websockets_chatserver/WebsocketsChatserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.websockets_chatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsChatserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsChatserverApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/websockets_chatserver/controller/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.websockets_chatserver.controller.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
